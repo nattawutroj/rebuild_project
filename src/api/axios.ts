77b9beabaa37673292cf5f5979d549472c1fbce6
nattawut rojchanachan <nattawutroj@gmail.com>
2024-05-04 17:23:10 +0700

    Update baseURL in axios.ts and Axios.js to use the production backend URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://spitech-backend.azurewebsites.net',
 });
 
 axiosInstance.interceptors.request.use(
